72aa90cdc950dc4204337caaacdf25afe05cc098
ksanu4221 <ksanu4221@gmail.com>
2025-02-05 22:58:15 +0530

    array of objects and destructuring

---
+++ ArrayOfObj.js
@@ -0,0 +1,23 @@
+
+
+
+// array of objects
+
+const array=[{"name":"sanu"},{"age":27},{"add":"banglore"}]
+
+const[a,b,c]=array
+    
+    console.log(array[2].add);
+
+// destructuring of object
+
+const obj={'names':"sanu","age":27}
+
+const{names,age}=obj
+
+
+console.log(obj.names);
+console.log(age);
+
+
+    
---
+++ object.js
@@ -0,0 +1,23 @@
+const array =["kumar","sanu",1,2]
+
+// re-assign
+// array=["kumar","sanu",1,2]
+
+
+array[1]="bhanu"
+array.push("naina")
+array.pop()
+console.log(array);
+
+// objects
+// keys and values pair
+const obj={"name":"sanu","age":"27"}
+
+console.log(typeof(obj.age));
+// re-declaration
+//  var obj ={"name":"naveesha"}
+
+obj.name ="naina"
+console.log(obj);
+
+
